47a3a29cefb7233bbb9c5df760a9e30566b6224d
YadavGaya031 <singhgaya031@gmail.com>
2025-10-07 01:20:49 +0530

    captain's login-logout completed.

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blackListTokenModel = require('../models/blacklistToken.model');
+const captainModel = require('../models/captain.models');
 
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
@@ -8,7 +10,7 @@ module.exports.authUser = async (req, res, next) => {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
-    const isBlacklisted = await userModel.findOne({ token });
+    const isBlacklisted = await blackListTokenModel.findOne({ token });
     if(isBlacklisted){
         return res.status(401).json({ message: 'Token has been blacklisted. Please log in again.' });
     }
@@ -22,3 +24,26 @@ module.exports.authUser = async (req, res, next) => {
     }
 }
  
+
+module.exports.authCaptain = async (req, res, next)=>{
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1];
+    if(!token){
+        return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+
+    const isBlacklisted = await blackListTokenModel.findOne({ token });
+    
+    if(isBlacklisted){
+        return res.status(401).json({ message: "Token has been blacklisted. Please log in again." });
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        // console.log(decoded._id)
+        req.captain = await captainModel.findById(decoded._id);
+        next();
+    }catch(err){
+        // console.log(err);
+        res.status(400).json({ message: "Invalid Token." });
+    }
+}
